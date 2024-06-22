be5cdec66c48d2b4a56ef7578a69c7165212f6a3
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-06-22 07:33:47 +0500

    mesage timing added

--- app.js
+++ app.js
@@ -4,12 +4,42 @@ var swiper = "";
 var list = document.getElementById("list")
 var typing = document.getElementById("typing")
 var match = "first";
+
+
 function sendMessage(){
     var count = 0;
 if(event.keyCode === 13){
     var elem = event.target;
     store = elem.value.toLowerCase();
-    list.innerHTML += `<div class='message sent'>` + elem.value + `<span class="metadata"><span class="time"></span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span></span></div> ` 
+    list.innerHTML += `<div class='message sent'>` + elem.value + `<span class="metadata"><span class="time">${moment().format('LT')}</span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span></span></div> ` 
+    
+//hhh
+function appendMessage() {
+	const message = document.getElementsByClassName('message')[0];
+}
+
+function getlist() {
+	// Prior to getting your list.
+  shouldScroll = list.scrollTop + list.clientHeight === list.scrollHeight;
+  /*
+   * Get your list, we'll just simulate it by appending a new one syncronously.
+   */
+  appendMessage();
+  // After getting your list.
+  if (!shouldScroll) {
+    scrollToBottom();
+  }
+}
+
+function scrollToBottom() {
+  list.scrollTop = list.scrollHeight;
+}
+
+scrollToBottom();
+
+setInterval(getlist, 100);
+//hhh
+
     // typing.style.display = "block";
     var arr = "abcdefghijklmnopqrstuvwxyz"
     var arr1 = ['hel','al','sal'];
@@ -55,48 +85,52 @@ if(event.keyCode === 13){
     }
     if(count == 1){
         setTimeout(function(){
-            list.innerHTML +=`<div class="message received"><span id="random"> oho </span><span class="metadata"><span class="time"></span></span></div>`
+            list.innerHTML +=`<div class="message received"><span id="random"> oho </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>`
         }, 1650)
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random">gm hgyi apki? </span><span class="metadata"><span class="time"></span></span></div>`  
+            list.innerHTML += `<div class="message received"><span id="random">gm hgyi apki? </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>`  
             // typing.style.display = "none";
         }, 3200)
     }
     else if(count == 2){
         match = "second";
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random"> hmm... wese aik bt khn ?  </span><span class="metadata"><span class="time"></span></span></div>` 
+            list.innerHTML += `<div class="message received"><span id="random"> hmm... wese aik bt khn ?  </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>` 
         }, 2000)
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random"> mjhy na tmse kch pxhna tha </span><span class="metadata"><span class="time"></span></span></div>` 
+            list.innerHTML += `<div class="message received"><span id="random"> mjhy na tmse kch pxhna tha </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>` 
         }, 4100)
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random"> wrning: no no's allowed </span><span class="metadata"><span class="time"></span></span></div>`  
+            list.innerHTML += `<div class="message received"><span id="random"> wrning: no no's allowed </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>`  
             // typing.style.display = "none";
         }, 5500)
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random"> plz plz plz... </span><span class="metadata"><span class="time"></span></span></div>`  
+            list.innerHTML += `<div class="message received"><span id="random"> plz plz plz... </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>`  
             // typing.style.display = "none";
         }, 6095)
     }
     else if(count == 3){
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random"> ni chrdo </span><span class="metadata"><span class="time"></span></span></div>`
+            list.innerHTML += `<div class="message received"><span id="random"> ni chrdo </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>`
         }, 2000)
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random"> tm yqin ni kroge </span><span class="metadata"><span class="time"></span></span></div>`
+            list.innerHTML += `<div class="message received"><span id="random"> tm yqin ni kroge </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>`
             // typing.style.display = "none";
         }, 4100)
     }
     else{
         setTimeout(function(){
-            list.innerHTML += `<div class="message received"><span id="random"> dkho fzul hrktein mt kro plz </span><span class="metadata"><span class="time"></span></span></div>`
+            list.innerHTML += `<div class="message received"><span id="random"> dkho fzul hrktein mt kro plz </span><span class="metadata"><span class="time">${moment().format('LT')}</span></span></div>`
             // typing.style.display = "none";
         }, 2000)
     }
+
+
     elem.value = ""
 }
 }
+console.log(moment().format('LT'))
+
 // /* Meme */
 
 // var memes = [
