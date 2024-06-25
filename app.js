43402937a9111cdfa530f02e6aff57f0cc741310
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-06-25 14:33:38 +0500

    scroll too mottom removed

--- app.js
+++ app.js
@@ -13,32 +13,32 @@ if(event.keyCode === 13){
     store = elem.value.toLowerCase();
     list.innerHTML += `<div class='message sent'>` + elem.value + `<span class="metadata"><span class="time">${moment().format('LT')}</span><span class="tick"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="15" id="msg-dblcheck-ack" x="2063" y="2076"><path d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.88a.32.32 0 0 1-.484.032l-.358-.325a.32.32 0 0 0-.484.032l-.378.48a.418.418 0 0 0 .036.54l1.32 1.267a.32.32 0 0 0 .484-.034l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.88a.32.32 0 0 1-.484.032L1.892 7.77a.366.366 0 0 0-.516.005l-.423.433a.364.364 0 0 0 .006.514l3.255 3.185a.32.32 0 0 0 .484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z" fill="#4fc3f7"/></svg></span></span></div> ` 
     
-//hhh
-function appendMessage() {
-	const message = document.getElementsByClassName('message')[0];
-}
+// //hhh
+// function appendMessage() {
+// 	const message = document.getElementsByClassName('message')[0];
+// }
 
-function getlist() {
-	// Prior to getting your list.
-  shouldScroll = list.scrollTop + list.clientHeight === list.scrollHeight;
-  /*
-   * Get your list, we'll just simulate it by appending a new one syncronously.
-   */
-  appendMessage();
-  // After getting your list.
-  if (!shouldScroll) {
-    scrollToBottom();
-  }
-}
+// function getlist() {
+// 	// Prior to getting your list.
+//   shouldScroll = list.scrollTop + list.clientHeight === list.scrollHeight;
+//   /*
+//    * Get your list, we'll just simulate it by appending a new one syncronously.
+//    */
+//   appendMessage();
+//   // After getting your list.
+//   if (!shouldScroll) {
+//     scrollToBottom();
+//   }
+// }
 
-function scrollToBottom() {
-  list.scrollTop = list.scrollHeight;
-}
+// function scrollToBottom() {
+//   list.scrollTop = list.scrollHeight;
+// }
 
-scrollToBottom();
+// scrollToBottom();
 
-setInterval(getlist, 100);
-//hhh
+// setInterval(getlist, 100);
+// //hhh
 
     // typing.style.display = "block";
     var arr = "abcdefghijklmnopqrstuvwxyz"
